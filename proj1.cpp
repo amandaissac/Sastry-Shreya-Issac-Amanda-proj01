1862b5514705906b240621ad5b5345a588e212ae
Shreya Sastry <srs93414@uga.edu>
2019-09-17 18:13:29 -0400

    added method to reduce length

--- proj1.cpp
+++ proj1.cpp
@@ -9,6 +9,7 @@
 #include <iterator>
 
 using namespace std;
+
 bool second=true;
 bool third=true;
 bool fourth=true;
@@ -18,6 +19,7 @@ int count=0;
 bool noA,noB,noC,noD,noE;
 vector<int>vec={-1,-1,-1,-1,-1};
 vector<vector<int>>all; //a vector holding vector memory
+void secondNum(int a,int b,int c,int d,int e,int third,int z);
 string bitwise(int total,vector<int>v){
     a=v[0];
     b=v[1];
@@ -30,147 +32,175 @@ string bitwise(int total,vector<int>v){
         noC=true;
         noD=true;
         noE=true;
-	//vec.clear();
+        //vec.clear();
         vec={-1,-1,-1,-1,-1};
         if(x==0){
-	  vec[0]=a;
-	  noA=false;
+            vec[0]=a;
+            noA=false;
         }
         if(x==1){
-	  vec[0]=b;
-	  noB=false;
+            vec[0]=b;
+            noB=false;
         }
         if(x==2){
-	  vec[0]=c;
-	  noC=false;
+            vec[0]=c;
+            noC=false;
             
         }
         if(x==3){
-	  vec[0]=d;
-	  noD=false;
+            vec[0]=d;
+            noD=false;
         }
         if(x==4){
-	  vec[0]=e;
-	  noE=false;
+            vec[0]=e;
+            noE=false;
         }
         //cout<<vec[0]<<endl;
         //figuring out second space
         for(int y=0;y<5;y+=1){
             second=true;
             if((y==0)&&(vec[0]!=a)){
-	      //vec.push_back(a);
-	      vec[1]=a;
-	      noA=false;
+                //vec.push_back(a);
+                vec[1]=a;
+                noA=false;
             }
             else if((y==1)&&(vec[0]!=b)){
-	      vec[1]=b;
-	      noB=false;
+                vec[1]=b;
+                noB=false;
             }
             else if((y==2)&&(vec[0]!=c)){
-	      vec[1]=c;
-	      noC=false;
+                vec[1]=c;
+                noC=false;
             }
             else if((y==3)&&(vec[0]!=d)){
-	      vec[1]=d;
-	      noD=false;
+                vec[1]=d;
+                noD=false;
             }
             else if((y==4)&&(vec[0]!=e)){
-	      vec[1]=e;
-	      noE=false;
+                vec[1]=e;
+                noE=false;
             }
             else{
                 second=false;
             }
-	    //after we have figured out the second one... within loop
-	    if(second){
-	      //cout<<vec[0]<<" "<<vec[1]<<endl;
-	      for(int z=0;z<5;z+=1){
-		third=true;
-		//cout<<"This is Z: "<<z<<endl;
-		if((z==0)&&(vec[0]!=a)&&(vec[1]!=a)){
-		  vec[2]=a;
-		}
-		else if((z==1)&&(vec[0]!=b)&&(vec[1]!=b)){
-		  vec[2]=b;
-		}
-		else if((z==2)&&(vec[0]!=c)&&(vec[1]!=c)){
-		  vec[2]=c;
-		}
-		else if((z==3)&&(vec[0]!=d)&&(vec[1]!=d)){
-		  vec[2]=d;
-		}
-		else if((z==4)&&(vec[0]!=e)&&(vec[1]!=e)){
-		  vec[2]=e;
-		}
-		else{
-		  third=false;
-		}
-		//going onto the fourth
-		if(third){
-		  //cout<<vec[0]<<" "<<vec[1]<<" "<<vec[2]<<endl;
-		  //count+=1;
-		  for(int w=0;w<5;w++){
-		    fourth=true;
-		    if((w==0)&&(vec[0]!=a)&&(vec[1]!=a)&&(vec[2]!=a)){
-		      vec[3]=a;
-		    }
-		    else if((w==1)&&(vec[0]!=b)&&(vec[1]!=b)&&(vec[2]!=b)){
-		      vec[3]=b;
-		    }
-		    else if((w==2)&&(vec[0]!=c)&&(vec[1]!=c)&&(vec[2]!=c)){
-		      vec[3]=c;
-		    }
-		    else if((w==3)&&(vec[0]!=d)&&(vec[1]!=d)&&(vec[2]!=d)){
-		      vec[3]=d;
-		    }
-		    else if((w==4)&&(vec[0]!=e)&&(vec[1]!=e)&&(vec[2]!=e)){
-		      vec[3]=e;
-		    }
-		    else{
-		      fourth=false;
-		    }
-		    //if the fourth one is found! Going to fifth!
-		    if(fourth){
-		      //count+=1;
-		      //cout<<vec[0]<<" "<<vec[1]<<" "<<vec[2]<<" "<<vec[3]<<endl;
-		      ///*
-		      for(int h=0;h<5;h+=1){
-			fifth=true;
-			if((h==0)&&(vec[0]!=a)&&(vec[1]!=a)&&(vec[2]!=a)&&(vec[3]!=a)){
-			  vec[4]=a;
-			}
-			else if((h==1)&&(vec[0]!=b)&&(vec[1]!=b)&&(vec[2]!=b)&&(vec[3]!=b)){
-			  vec[4]=b;
-			}
-			else if((h==2)&&(vec[0]!=c)&&(vec[1]!=c)&&(vec[2]!=c)&&(vec[3]!=c)){
-			  vec[4]=c;
-			}
-			else if((h==3)&&(vec[0]!=d)&&(vec[1]!=d)&&(vec[2]!=d)&&(vec[3]!=d)){
-			  vec[4]=d;
-			}
-			else if((h==4)&&(vec[0]!=e)&&(vec[1]!=e)&&(vec[2]!=e)&&(vec[3]!=e)){
-			  vec[4]=e;
-			}
-			else{
-			  fifth=false;
-			}
-			if(fifth){
-			  all.push_back(vec);
-			}
-		      }//for h
-		      //*/
-		    }
-		  }//for for w
-		}
-	      }
-	    }//if second
+            //after we have figured out the second one... within loop
+            if(second){
+                              
+                //cout<<vec[0]<<" "<<vec[1]<<endl;
+                for(int z=0;z<5;z+=1){
+                    
+                    third=true;
+                    //cout<<"This is Z: "<<z<<endl;
+                    if((z==0)&&(vec[0]!=a)&&(vec[1]!=a)){
+                        vec[2]=a;
+                    }
+                    else if((z==1)&&(vec[0]!=b)&&(vec[1]!=b)){
+                        vec[2]=b;
+                    }
+                    else if((z==2)&&(vec[0]!=c)&&(vec[1]!=c)){
+                        vec[2]=c;
+                    }
+                    else if((z==3)&&(vec[0]!=d)&&(vec[1]!=d)){
+                        vec[2]=d;
+                    }
+                    else if((z==4)&&(vec[0]!=e)&&(vec[1]!=e)){
+                        vec[2]=e;
+                    }
+                    else{
+                        third=false;
+                    }
+                    
+                    // secondNum(a,b,c,d,e,third,z);
+                    //going onto the fourth
+                    if(third){
+                        //cout<<vec[0]<<" "<<vec[1]<<" "<<vec[2]<<endl;
+                        //count+=1;
+                        for(int w=0;w<5;w++){
+                            fourth=true;
+                            if((w==0)&&(vec[0]!=a)&&(vec[1]!=a)&&(vec[2]!=a)){
+                                vec[3]=a;
+                            }
+                            else if((w==1)&&(vec[0]!=b)&&(vec[1]!=b)&&(vec[2]!=b)){
+                                vec[3]=b;
+                            }
+                            else if((w==2)&&(vec[0]!=c)&&(vec[1]!=c)&&(vec[2]!=c)){
+                                vec[3]=c;
+                            }
+                            else if((w==3)&&(vec[0]!=d)&&(vec[1]!=d)&&(vec[2]!=d)){
+                                vec[3]=d;
+                            }
+                            else if((w==4)&&(vec[0]!=e)&&(vec[1]!=e)&&(vec[2]!=e)){
+                                vec[3]=e;
+                            }
+                            else{
+                                fourth=false;
+                            }
+                            //if the fourth one is found! Going to fifth!
+                            if(fourth){
+                                //count+=1;
+                                //cout<<vec[0]<<" "<<vec[1]<<" "<<vec[2]<<" "<<vec[3]<<endl;
+                                ///*
+                                for(int h=0;h<5;h+=1){
+                                    fifth=true;
+                                    if((h==0)&&(vec[0]!=a)&&(vec[1]!=a)&&(vec[2]!=a)&&(vec[3]!=a)){
+                                        vec[4]=a;
+                                    }
+                                    else if((h==1)&&(vec[0]!=b)&&(vec[1]!=b)&&(vec[2]!=b)&&(vec[3]!=b)){
+                                        vec[4]=b;
+                                    }
+                                    else if((h==2)&&(vec[0]!=c)&&(vec[1]!=c)&&(vec[2]!=c)&&(vec[3]!=c)){
+                                        vec[4]=c;
+                                    }
+                                    else if((h==3)&&(vec[0]!=d)&&(vec[1]!=d)&&(vec[2]!=d)&&(vec[3]!=d)){
+                                        vec[4]=d;
+                                    }
+                                    else if((h==4)&&(vec[0]!=e)&&(vec[1]!=e)&&(vec[2]!=e)&&(vec[3]!=e)){
+                                        vec[4]=e;
+                                    }
+                                    else{
+                                        fifth=false;
+                                    }
+                                    if(fifth){
+                                        all.push_back(vec);
+                                    }
+                                }//for h
+                                //*/
+                            }
+                        }//for for w
+                    }
+                }
+            }//if second
         }          
     }
     count=all.size(); //count is the size
     for(int q=0;q<count;q+=1){
-      v=all[q]; //setting v as temperory vector at iteration q
-      cout<<v[0]<<" "<<v[1]<<" "<<v[2]<<" "<<v[3]<<" "<<v[4]<<endl;
+        v=all[q]; //setting v as temperory vector at iteration q
+        cout<<v[0]<<" "<<v[1]<<" "<<v[2]<<" "<<v[3]<<" "<<v[4]<<endl;
     }
+    
     return "";
+}
+void secondNum(int a,int b,int c,int d,int e,int third, int z){
+    third=true;
+//cout<<"This is Z: "<<z<<endl;
+    if((z==0)&&(vec[0]!=a)&&(vec[1]!=a)){
+        vec[2]=a;
+    }
+    else if((z==1)&&(vec[0]!=b)&&(vec[1]!=b)){
+        vec[2]=b;
+    }
+    else if((z==2)&&(vec[0]!=c)&&(vec[1]!=c)){
+                        vec[2]=c;
+    }
+    else if((z==3)&&(vec[0]!=d)&&(vec[1]!=d)){
+        vec[2]=d;
+                    }
+    else if((z==4)&&(vec[0]!=e)&&(vec[1]!=e)){
+        vec[2]=e;
+    }
+    else{
+        third=false;
+    }    
+    return;
 }
 
