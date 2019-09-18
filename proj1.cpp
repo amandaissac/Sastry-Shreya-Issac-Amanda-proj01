4c7f3e2671308fe38aa307d9b88473d38808e779
Amanda Issac <ari31551@uga.edu>
2019-09-18 01:58:21 -0400

    added the correct operaters. need to fix order

--- proj1.cpp
+++ proj1.cpp
@@ -17,10 +17,11 @@ bool fifth=true;
 int a,b,c,d,e;
 int count=0;
 bool noA,noB,noC,noD,noE;
-string ari;
+vector<string> ari={"","","",""};
 int op1,op2,op3,op4;
 vector<int>vec={-1,-1,-1,-1,-1};
 vector<vector<int>>all; //a vector holding vector memory
+vector<string>output; //holding all the output strings
 void secondNum(int a,int b,int c,int d,int e,int third,int z);
 string bitwise(int total,vector<int>v){
     a=v[0];
@@ -180,111 +181,114 @@ string bitwise(int total,vector<int>v){
       //operand 1
       for(int a1=0;a1<5;a1++){
 	//resetting
-	ari="";
+	//ari="";
 	op1=0;
 	op2=0;
 	op3=0;
 	op4=0;
 	if(a1==0){
 	  op1=v[0]+v[1];
-	  ari+="+";
+	  ari[0]="+";
 	}
 	if(a1==1){
 	  op1=v[0]-v[1];
-	  ari+="-";
+	  ari[0]="-";
 	}
 	if(a1==2){
 	  op1=v[0]*v[1];
-	  ari+="*";
+	  ari[0]="*";
 	}
 	if((a1==3)&&(v[1]!=0)){
 	  op1=v[0]/v[1];
-	  ari+="/";
+	  ari[0]="/";
 	}
 	if((a1==4)&&(v[1]!=0)){
 	  op1=v[0]%v[1];
-	  ari+="%";
+	  ari[0]="%";
 	}
 	//cout<<"Operand 1: "<<ari<<endl;
 	//got operand 1; Now to operand 2
 	for(int a2=0;a2<5;a2++){
 	  if(a2==0){
 	    op2=op1+v[2];
-	    ari+="+";
+	    ari[1]="+";
 	  }
 	  if(a2==1){
 	    op2=op1-v[2];
-	    ari+="-";
+	    ari[1]="-";
 	  }
 	  if(a2==2){
 	    op2=op1*v[2];
-	    ari+="*";
+	    ari[1]="*";
 	  }
 	  if((a2==3)&&(v[2]!=0)){
 	    op2=op1/v[2];
-	    ari+="/";
+	    ari[1]="/";
 	  }
 	  if((a2==4)&&(v[2]!=0)){
 	    op2=op1%v[2];
-	    ari+="%";
+	    ari[1]="%";
 	  }
 	  //cout<<"Operand 2: "<<ari<<endl;
 	  //got operand 2; Now to operand 3
 	  for(int a3=0;a3<5;a3++){
 	    if(a3==0){
 	      op3=op2+v[3];
-	      ari+="+";
+	      ari[2]="+";
 	    }
 	    if(a3==1){
 	      op3=op2-v[3];
-	      ari+="-";
+	      ari[2]="-";
 	    }
 	    if(a3==2){
 	      op3=op2*v[3];
-	      ari+="*";
+	      ari[2]="*";
 	    }
 	    if((a3==3)&&(v[3]!=0)){
 	      op3=op2/v[3];
-	      ari+="/";
+	      ari[2]="/";
 	    }
 	    if((a3==4)&&(v[3]!=0)){
 	      op3=op2%v[3];
-	      ari+="%";
+	      ari[2]="%";
 	    }
 	    //cout<<"Operand 3: "<<ari<<endl;
 	    //got operand 3; Now operand 4
 	    for(int a4=0;a4<5;a4++){
 	      if(a4==0){
 	      op4=op3+v[4];
-	      ari+="+";
+	      ari[3]="+";
 	      }
 	      if(a4==1){
 		op4=op3-v[4];
-		ari+="-";
+		ari[3]="-";
 	      }
 	      if(a4==2){
 		op4=op3*v[4];
-		ari+="*";
+		ari[3]="*";
 	      }
 	      if((a4==3)&&(v[4]!=0)){
 		op4=op3/v[4];
-		ari+="/";
+		ari[3]="/";
 	      }
 	      if((a4==4)&&(v[4]!=0)){
 		op4=op3%v[4];
-		ari+="%";
+		ari[3]="%";
 	      }
 	      //cout<<"Operand 4: "<<ari<<endl;
 	      //all 4 operands have been found
 	      if(op4==total){
+		output.push_back("((("+to_string(v[0])+ari[0]+to_string(v[1])+")"+ari[1]+to_string(v[2])+")"+ari[2]+to_string(v[3])+")"+ari[3]+to_string(v[4]));
 		count+=1;
-
-        //cout<<"Ari: "<<ari<<endl;
+		//cout<<"Ari: "<<ari<<endl;
+		/*
 		if(count<10){
-            //cout<<"Ari: "<<ari<<endl;
-            cout<<"((("<<v[0]<<ari.at(0)<<v[1]<<")"<<ari.at(1)<<v[2]<<")"<<ari.at(2)<<v[3]<<")"<<ari.at(3)<<v[4]<<endl;
+		  //cout<<"Ari: "<<ari<<endl;
+		  cout<<"((("<<v[0]<<ari[0]<<v[1]<<")"<<ari[1]<<v[2]<<")"<<ari[2]<<v[3]<<")"<<ari[3]<<v[4]<<endl;
 		}
+		*/
 	      }
+	      //ari=""; //resetting ari after reaching 4th operand, etc.
 	    }//for a4
 	  }
 	}
@@ -303,6 +307,16 @@ string bitwise(int total,vector<int>v){
     cout<<"Should be 120: "<<count<<endl;
     */
     //cout<<"Ari: "<<ari<<endl;
+    if(count<10){
+      for(int s=0;s<count;s++){
+	cout<<output[s]<<endl;
+      }
+    }
+    else{
+      for(int s=count-10;s<count;s++){
+	cout<<output[s]<<endl;
+      }
+    }
     cout<<count<<" arithmetic expressions found that evaluate to "<<total<<endl;
     return "";
 }
